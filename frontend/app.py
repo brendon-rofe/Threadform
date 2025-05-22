796bd89994702e923130ec22f22b4036b7fc4749
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-05-22 15:11:23 +0200

    feat: Implement initial chat functionality

--- frontend/app.py
+++ frontend/app.py
@@ -3,12 +3,21 @@ from dotenv import load_dotenv
 import os
 from google import genai
 
-load_dotenv()
-
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=GEMINI_API_KEY)
 
-st.title("Threadform")
+load_dotenv()
+
+def generate_response(prompt):
+    response = client.models.generate_content_stream(
+        model="gemini-2.0-flash",
+        contents=[{"role": "user", "parts": [{"text": prompt}]}]
+    )
+    for chunk in response:
+        if chunk.text:
+            yield chunk.text
+
+st.title("Threadform MVP")
 
 prompt = st.chat_input("Ask anything")
 
@@ -17,4 +26,4 @@ if prompt:
     st.markdown(prompt)
 
   with st.chat_message("assistant"):
-    st.write(f"The user entered the following prompt: {prompt}")
+    st.write_stream(generate_response(prompt))
